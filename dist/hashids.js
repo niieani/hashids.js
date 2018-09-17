1b566c6f629060a464634987e580edf4e6fed3be
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2018-09-17 22:33:24 +0200

    fix order in which we assign values (fixes #56)
    wrong order was causing babel interop to fail

--- dist/hashids.js
+++ dist/hashids.js
@@ -2,5 +2,5 @@
 // this file here is for backwards compatibility with an earlier CommonJS version
 const Hashids = require("./index").default;
 Object.defineProperty(Hashids, "__esModule", {value: true});
-Hashids.default = module.exports;
 module.exports = Hashids;
+Hashids.default = module.exports;
