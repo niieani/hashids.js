402835eda6d19a926621a03c7aea9a24decc2c41
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2019-06-23 02:53:10 +0200

    typescript + fix bug
    .

--- lib/hashids.ts
+++ lib/hashids.ts
@@ -10,84 +10,108 @@ export default class Hashids {
     seps = 'cfhistuCFHISTU',
   ) {
     if (typeof minLength !== 'number') {
-      throw new Error(`Provided 'minLength' has to be a number (is ${typeof minLength})`)
+      throw new Error(
+        `Provided 'minLength' has to be a number (is ${typeof minLength})`,
+      )
     }
     if (typeof salt !== 'string') {
       throw new Error(`Provided 'salt' has to be a string (is ${typeof salt})`)
     }
     if (typeof alphabet !== 'string') {
-      throw new Error(`Provided alphabet has to be a string (is ${typeof alphabet})`)
+      throw new Error(
+        `Provided alphabet has to be a string (is ${typeof alphabet})`,
+      )
     }
 
-    /** `alphabet` should not contains `seps` */
-    const filteredAlpabet = withoutChars(alphabet, seps)
-    this.alphabet = keepUniqueChars(filteredAlpabet)
-
-    if (this.alphabet.length < minAlphabetLength) {
-      throw new Error(`error: alphabet must contain at least ${minAlphabetLength} unique characters`)
-    }
-
-    if (this.alphabet.indexOf(' ') !== -1) {
+    if (alphabet.indexOf(' ') !== -1) {
       throw new Error('error: alphabet cannot contain spaces')
     }
 
+    const uniqueAlphabet = keepUniqueChars(alphabet)
+
+    if (uniqueAlphabet.length < minAlphabetLength) {
+      throw new Error(
+        `error: alphabet must contain at least ${minAlphabetLength} unique characters, provided: ${uniqueAlphabet}`,
+      )
+    }
+
+    /** `alphabet` should not contains `seps` */
+    this.alphabet = withoutChars(uniqueAlphabet, seps)
     /** `seps` should contain only characters present in `alphabet` */
-    const filteredSeps = withoutChars(seps, this.alphabet)
+    const filteredSeps = onlyChars(seps, uniqueAlphabet)
     this.seps = shuffle(filteredSeps, salt)
 
     let sepsLength
     let diff
 
-    if (!this.seps.length || (this.alphabet.length / this.seps.length) > sepDiv) {
-      sepsLength = Math.ceil(this.alphabet.length / sepDiv)
+    if (
+      [...this.seps].length === 0 ||
+      [...this.alphabet].length / [...this.seps].length > sepDiv
+    ) {
+      sepsLength = Math.ceil([...this.alphabet].length / sepDiv)
 
-      if (sepsLength > this.seps.length) {
-        diff = sepsLength - this.seps.length
+      if (sepsLength > [...this.seps].length) {
+        diff = sepsLength - [...this.seps].length
         this.seps += unicodeSubstr(this.alphabet, 0, diff)
         this.alphabet = unicodeSubstr(this.alphabet, diff)
       }
     }
 
     this.alphabet = shuffle(this.alphabet, salt)
-    const guardCount = Math.ceil(this.alphabet.length / guardDiv)
+    const guardCount = Math.ceil([...this.alphabet].length / guardDiv)
 
-    if (this.alphabet.length < 3) {
-      this.guards = unicodeSubstr(seps, 0, guardCount)
-      this.seps = unicodeSubstr(seps, guardCount)
+    if ([...this.alphabet].length < 3) {
+      this.guards = unicodeSubstr(this.seps, 0, guardCount)
+      this.seps = unicodeSubstr(this.seps, guardCount)
     } else {
-      this.guards = unicodeSubstr(alphabet, 0, guardCount)
-      this.alphabet = unicodeSubstr(alphabet, guardCount)
+      this.guards = unicodeSubstr(this.alphabet, 0, guardCount)
+      this.alphabet = unicodeSubstr(this.alphabet, guardCount)
     }
   }
 
-  encode(...numbers) {
+  encode(numbers: string): string
+  // eslint-disable-next-line no-dupe-class-members
+  encode(numbers: Array<number>): string
+  // eslint-disable-next-line no-dupe-class-members
+  encode(...numbers: Array<number>): string
+  // eslint-disable-next-line no-dupe-class-members
+  encode(numbers: Array<string>): string
+  // eslint-disable-next-line no-dupe-class-members
+  encode(...numbers: Array<string>): string
+  // eslint-disable-next-line no-dupe-class-members
+  encode<T extends string | number>(
+    first: Array<T> | T,
+    ...numbers: Array<T>
+  ): string {
     const ret = ''
+
+    if (Array.isArray(first)) {
+      // @ts-ignore
+      numbers = first
+    } else {
+      // eslint-disable-next-line eqeqeq
+      numbers = [...(first != null ? [first] : []), ...numbers]
+    }
 
     if (!numbers.length) {
       return ret
     }
 
-    if (numbers[0] && numbers[0].constructor === Array) {
-      numbers = numbers[0]
-      if (!numbers.length) {
-        return ret
-      }
+    if (!numbers.every(isIntegerNumber)) {
+      // @ts-ignore
+      numbers = numbers.map((n) => safeParseInt10(String(n), 10))
     }
 
-    for (let i = 0; i !== numbers.length; i++) {
-      numbers[i] = _parseInt(numbers[i], 10)
-      if (numbers[i] >= 0) {
-        continue
-      } else {
-        return ret
-      }
+    // @ts-ignore
+    if (!numbers.every(isPositiveAndFinite)) {
+      return ret
     }
 
-    return this._encode(numbers)
+    return this._encode(numbers as Array<number>)
   }
 
   decode(id: string) {
-    const ret = []
+    const ret: Array<number> = []
 
     if (!id || !id.length || typeof id !== 'string') {
       return ret
@@ -96,42 +120,34 @@ export default class Hashids {
     return this._decode(id, this.alphabet)
   }
 
-  encodeHex(hex) {
+  encodeHex(hex: string): string {
     hex = hex.toString()
     if (!/^[0-9a-fA-F]+$/.test(hex)) {
       return ''
     }
 
     const numbers = hex.match(/[\w\W]{1,12}/g)
+    if (!numbers) return ''
 
-    for (let i = 0; i !== numbers.length; i++) {
-      numbers[i] = parseInt('1' + numbers[i], 16)
-    }
-
-    return this.encode.apply(this, numbers)
+    const decNumbers = Array.from(numbers, (value) => parseInt(`1${value}`, 16))
+    return this.encode(decNumbers)
   }
 
-  decodeHex(id) {
-    let ret = []
-
+  decodeHex(id: string) {
     const numbers = this.decode(id)
-
-    for (let i = 0; i !== numbers.length; i++) {
-      ret += numbers[i].toString(16).substr(1)
-    }
-
-    return ret
+    return numbers.map((number) => number.toString(16).substr(1)).join('')
   }
 
-  _encode(numbers) {
-    let ret,
-      alphabet = this.alphabet,
-      numbersIdInt = 0
+  _encode(numbers: Array<number>): string {
+    let ret: string
+    let alphabet = this.alphabet
+    let numbersIdInt = 0
 
     for (let i = 0; i !== numbers.length; i++) {
       numbersIdInt += numbers[i] % (i + 100)
     }
 
+    ret = alphabet.charAt(numbersIdInt % alphabet.length)
     const lottery = ret
 
     for (let i = 0; i !== numbers.length; i++) {
@@ -165,7 +181,7 @@ export default class Hashids {
       }
     }
 
-    const halfLength = parseInt(alphabet.length / 2, 10)
+    const halfLength = Math.floor(alphabet.length / 2)
     while (ret.length < this.minLength) {
       alphabet = shuffle(alphabet, alphabet)
       ret = alphabet.substr(halfLength) + ret + alphabet.substr(0, halfLength)
@@ -179,12 +195,12 @@ export default class Hashids {
     return ret
   }
 
-  _decode(id, alphabet) {
-    let ret = [],
-      i = 0,
-      r = new RegExp(`[${escapeRegExp(this.guards)}]`, 'g'),
-      idBreakdown = id.replace(r, ' '),
-      idArray = idBreakdown.split(' ')
+  _decode(id: string, alphabet: string): Array<number> {
+    let ret: Array<number> = []
+    let i = 0
+
+    let idBreakdown = replaceGuardsWithSpaces(id, this.guards)
+    let idArray = idBreakdown.split(' ')
 
     if (idArray.length === 3 || idArray.length === 2) {
       i = 1
@@ -193,10 +209,7 @@ export default class Hashids {
     idBreakdown = idArray[i]
     if (typeof idBreakdown[0] !== 'undefined') {
       const lottery = idBreakdown[0]
-      idBreakdown = idBreakdown.substr(1)
-
-      r = new RegExp(`[${escapeRegExp(this.seps)}]`, 'g')
-      idBreakdown = idBreakdown.replace(r, ' ')
+      idBreakdown = replaceGuardsWithSpaces(idBreakdown.substr(1), this.seps)
       idArray = idBreakdown.split(' ')
 
       for (let j = 0; j !== idArray.length; j++) {
@@ -220,15 +233,24 @@ const minAlphabetLength = 16
 const sepDiv = 3.5
 const guardDiv = 12
 
-export const keepUniqueChars = (str: string) => Array.from(new Set(str)).join('')
+export const keepUniqueChars = (str: string) =>
+  Array.from(new Set(str)).join('')
 
 export const withoutChars = ([...str]: string, [...without]: string) =>
   str.filter((char) => !without.includes(char)).join('')
 
-export const unicodeSubstr = ([...str]: string, from: number, to?: number) => str.slice(from, to).join('')
+export const onlyChars = ([...str]: string, [...only]: string) =>
+  str.filter((char) => only.includes(char)).join('')
+
+export const unicodeSubstr = ([...str]: string, from: number, to?: number) =>
+  str.slice(from, to).join('')
+
+const isIntegerNumber = (n: number | string) =>
+  !Number.isNaN(Number(n)) && Math.floor(Number(n)) === n
+const isPositiveAndFinite = (n: number) => n >= 0 && Number.isFinite(n)
 
 function shuffle(alphabet: string, salt: string) {
-  let integer
+  let integer: number
 
   if (!salt.length) {
     return alphabet
@@ -249,16 +271,12 @@ function shuffle(alphabet: string, salt: string) {
   return alphabetChars.join('')
 }
 
-const escapeRegExp = (s) => s.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&')
-const _parseInt = (v: string, radix: number) =>
-  /^(-|\+)?([0-9]+|Infinity)$/.test(v) ? parseInt(v, radix) : NaN
-
 const toAlphabet = (input: number, alphabet: string) => {
   let id = ''
 
   do {
     id = alphabet.charAt(input % alphabet.length) + id
-    input = parseInt(input / alphabet.length, 10)
+    input = Math.floor(input / alphabet.length)
   } while (input)
 
   return id
@@ -268,3 +286,10 @@ const fromAlphabet = (input: string, alphabet: string) =>
   [...input]
     .map((item) => alphabet.indexOf(item))
     .reduce((carry, item) => carry * alphabet.length + item, 0)
+
+const replaceGuardsWithSpaces = ([...chars]: string, guards: string) =>
+  chars.map((char) => (guards.includes(char) ? ' ' : char)).join('')
+
+const safeNumberRegExp = /^(-|\+)?([0-9]+|Infinity)$/
+const safeParseInt10 = (str: string) =>
+  safeNumberRegExp.test(str) ? parseInt(str, 10) : NaN
