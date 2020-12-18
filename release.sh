4e5a2bab58cc6af340df8743d6c8ea8dce89ab8f
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2020-12-18 01:20:31 -0800

    ci: run prettier and lint check + commit dist files

---
+++ release.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+if [[ -z "$GITHUB_REPOSITORY" ]]; then
+  echo "Release step must be run inside of a GitHub Action."
+  exit 1
+fi
+
+set -o xtrace
+
+declare BRANCH="${GITHUB_REF#*/*/}"
+
+git remote set-url origin "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+git config --global user.email "action@github.com"
+git config --global user.name "GitHub Action"
+
+./yarn prettier:write
+git add lib
+git commit -m "style: apply prettier" || true
+
+./yarn build
+git add .
+git commit -m "dist: build release" || true
+
+git push origin "HEAD:$BRANCH"
+
+# NPM_CONFIG_USERCONFIG is set by the GitHub action, we want to use the local .npmrc downloaded above ^
+NPM_CONFIG_USERCONFIG='' ./yarn semantic-release "$@"
