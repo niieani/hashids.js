f109d55ce42b3f9bcb705e58d2a0b503652f38e2
Frederik Wallner <frederik.wallner@gmail.com>
2023-02-16 21:36:59 +0100

    fix: add ".js" extension to type definition import (#535)

--- add-extension.sh
+++ add-extension.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-declare file="esm/hashids.js"
+function addJsExtension() {
+  declare file="$1"
+  declare replacement="from './util.js'"
+  declare file_contents
+  file_contents=$(<$file)
+  echo "${file_contents//from \'.\/util\'/${replacement}}" >"$file"
+}
 
-declare file_contents
-file_contents=$(<$file)
-
-declare replacement="from './util.js'"
-
-echo "${file_contents//from \'.\/util\'/${replacement}}" >"$file"
+addJsExtension "esm/hashids.js"
+addJsExtension "esm/hashids.d.ts"
